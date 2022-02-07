b36c1559f976faf72b9ab421a2b985110cf037ad
FSProLovish <lavishgarg006@gmail.com>
2022-02-07 12:05:06 +0530

    07-02-2022

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,11 +1,14 @@
+// get the Home Page
 module.exports.home = function (req, res) {
   return res.render("home");
 };
 
+// get the Create Contact Page
 module.exports.addContact = function (req, res) {
   return res.render("addContact");
 };
 
+// get the Update Contact Page
 module.exports.updateContact = function (req, res) {
   return res.render("updateContact", {
     id: req.query.id,
